e2d7c829982b8a8af6666f2876bfad3e47ce94ea
engels <engelmmanuel@gmail.com>
2021-09-24 00:19:46 +0100

    Started working on a task manager app with mongoDB

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,33 +1,14 @@
-// setTimeout(() => {
-//     console.log('Two seconds are up')
-// }, 2000)
-
-// const names = ['Engels', 'Andrew', 'Jen']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-//         callback(data)
-//     }, 2000)
-// }
-
-// geocode('Lagos', (data) => {
-//     console.log(data)
-// })
-
-const add = (a, b, callback) => {
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        const sum = a + b
-        callback(sum)
+        callback('This is my error', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum)
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
