d9249b29f87351702cd37d85ea98dbfb62b08dd6
engels <engelmmanuel@gmail.com>
2021-10-12 00:41:22 +0100

    Learnt testing in node.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,41 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
-const port = process.env.PORT || 3000
-
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+const app = require('./app')
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('6159d1c1ce3f8a13d884a31d')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('6159cf889e08d40ef51c92b2')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-main()
